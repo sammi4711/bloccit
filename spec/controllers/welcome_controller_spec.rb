22a3c7837006794da6614f5881032b2e3cef42aa
Samantha Acampora <samantha.acampora@gmail.com>
2019-05-22 22:24:21 -0400

    checkpoint rails testing complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "renders the index template" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template("index")
     end
   end
 
   describe "GET #about" do
-    it "returns http success" do
+    it "renders the about template" do
       get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template("about")
     end
   end
 
