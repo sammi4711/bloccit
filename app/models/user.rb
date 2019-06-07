2c31e9898babe60e8b9dac6498991d707a4c1ec7
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-07 14:31:08 -0400

    assignment authentication user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -12,4 +13,16 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def format_name
+    if name 
+      name_array = []
+      name.split.each do |firstlast|
+        name_array << firstlast.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+  end
+
 end
