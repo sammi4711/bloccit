35666cfe6af32b2a13ee687467c32aabc47e988e
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-07 13:43:31 -0400

    checkpoint authentication user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
   validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
             presence: true,
-            uniqueness: { case_sensitive: false },
+            uniqueness: { case_sensitive: true },
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
