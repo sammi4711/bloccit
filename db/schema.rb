efec31fc150c8b2bf66533dc3c3c69678b52dccd
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-10 17:16:32 -0400

    checkpoint posts and users complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_162253) do
+ActiveRecord::Schema.define(version: 2019_06_10_174945) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2019_06_07_162253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "topic_id"
+    t.integer "user_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
