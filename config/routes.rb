98e839f4756d169bbcf09f6fd9098cb3b7f7dd69
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-09 22:23:36 -0400

    ugly button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :topics do 
     resources :posts, except: [:index]
   end
+
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
+  
   get 'about' => 'welcome#about'
   root 'welcome#index'
 end
