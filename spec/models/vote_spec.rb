ea1d5e422b669102a785f03ca7448ac7c6bb3e78
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-18 17:10:32 -0400

    checkpoint voting complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -9,7 +9,12 @@ RSpec.describe Vote, type: :model do
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:value) }
-  it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
+#  it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
+  it do
+    should validate_inclusion_of(:value).
+    in_array([-1, 1]).
+    with_message("123456789 is not a valid vote.")
+  end
 
   describe "update_post callback" do
     it "triggers update_post on save" do
